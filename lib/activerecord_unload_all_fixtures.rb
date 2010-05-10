2f1141155f09ae4a40609b451b73a8e24b46345f
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2010-05-10 13:01:49 +0100

    consider models loaded after the first run of unload_all_fixtures

--- lib/activerecord_unload_all_fixtures.rb
+++ lib/activerecord_unload_all_fixtures.rb
@@ -10,34 +10,43 @@ module ActiveRecord
       # iterate over all ActiveRecord models associated with db tables, deleting all rows
       # if we're inside a transaction, use delete, otherwise use truncate
       def unload_all_fixtures() 
-        if ActiveRecord::UnloadAllFixtures::ordered_active_record_table_names.nil? || 
-            ActiveRecord::UnloadAllFixtures::ordered_active_record_table_names.empty?
-          klasses = ActiveRecord::Base.send(:subclasses).reject{ |klass| klass.skip_unload_fixtures if klass.respond_to?(:skip_unload_fixtures) }
-          ActiveRecord::UnloadAllFixtures::ordered_active_record_table_names = klasses.map do |klass|
-            if defined?(ActiveRecord::WormTable) && klass.ancestors.include?(ActiveRecord::WormTable)
-              [klass.switch_table_name] + klass.table_version_names
-            else
-              klass.table_name
-            end
-          end.flatten.to_set.to_a
+        if UnloadAllFixtures::ordered_active_record_table_names.nil? || 
+            UnloadAllFixtures::ordered_active_record_table_names.empty?
+          UnloadAllFixtures::ordered_active_record_table_names = activerecord_model_tables
+        else
+          # add newly defined models onto the end of the ordered list
+          UnloadAllFixtures::ordered_active_record_table_names += activerecord_model_tables - UnloadAllFixtures::ordered_active_record_table_names
         end
         
-        ActiveRecord::UnloadAllFixtures::table_name_set = ActiveRecord::Base::connection.tables.to_set
+        # get latest list of all tables
+        UnloadAllFixtures::table_name_set = ActiveRecord::Base::connection.tables.to_set
 
         # start with the last successful delete ordering, only re-ordering if new foreign key dependencies are found
         ActiveRecord::Base::without_foreign_key_checks do
-          ActiveRecord::UnloadAllFixtures::ordered_active_record_table_names = delete_rows( ActiveRecord::UnloadAllFixtures::ordered_active_record_table_names, 
+          UnloadAllFixtures::ordered_active_record_table_names = delete_rows( UnloadAllFixtures::ordered_active_record_table_names, 
                                                                             ActiveRecord::Base.connection.open_transactions == 0 )
         end
         
         true
       end
 
+      # all tables associated with ActiveRecord models, not marked for skip_unload_fixtures
+      def activerecord_model_tables
+        klasses = ActiveRecord::Base.send(:subclasses).reject{ |klass| klass.skip_unload_fixtures if klass.respond_to?(:skip_unload_fixtures) }
+        klasses.map do |klass|
+          if defined?(ActiveRecord::WormTable) && klass.ancestors.include?(ActiveRecord::WormTable)
+            [klass.switch_table_name] + klass.table_version_names
+          else
+            klass.table_name
+          end
+        end.flatten.to_set.to_a
+      end
+
       def delete_rows(table_names, truncate=false, shift_counter=table_names.size)
         processed = []
         table_names.each_with_index{ |table_name,i|
           begin
-            if ActiveRecord::UnloadAllFixtures::table_name_set.include?(table_name)
+            if UnloadAllFixtures::table_name_set.include?(table_name)
               if truncate
                 ActiveRecord::Base.connection.execute("truncate table #{table_name}")
               else
